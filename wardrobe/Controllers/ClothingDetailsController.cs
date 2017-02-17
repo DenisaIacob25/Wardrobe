e599e030ebc4de574c1e7748bf62f6c1ba37ba02
Denisa Iacob <denisaiacob18@gmail.com>
2017-02-16 19:06:10 -0500

    integrated db into VS and scaffolded

---
+++ wardrobe/Controllers/ClothingDetailsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using wardrobe.Models;
+
+namespace wardrobe.Controllers
+{
+    public class ClothingDetailsController : Controller
+    {
+        private wardrobeEntities db = new wardrobeEntities();
+
+        // GET: ClothingDetails
+        public ActionResult Index()
+        {
+            return View(db.ClothingDetails.ToList());
+        }
+
+        // GET: ClothingDetails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClothingDetail clothingDetail = db.ClothingDetails.Find(id);
+            if (clothingDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clothingDetail);
+        }
+
+        // GET: ClothingDetails/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClothingDetails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ClothingID,Title,Color,Photo,OccassionID,SeasonID,TypeID")] ClothingDetail clothingDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ClothingDetails.Add(clothingDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(clothingDetail);
+        }
+
+        // GET: ClothingDetails/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClothingDetail clothingDetail = db.ClothingDetails.Find(id);
+            if (clothingDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clothingDetail);
+        }
+
+        // POST: ClothingDetails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ClothingID,Title,Color,Photo,OccassionID,SeasonID,TypeID")] ClothingDetail clothingDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(clothingDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(clothingDetail);
+        }
+
+        // GET: ClothingDetails/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClothingDetail clothingDetail = db.ClothingDetails.Find(id);
+            if (clothingDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clothingDetail);
+        }
+
+        // POST: ClothingDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ClothingDetail clothingDetail = db.ClothingDetails.Find(id);
+            db.ClothingDetails.Remove(clothingDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
